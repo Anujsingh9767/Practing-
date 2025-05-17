438a619e4b4274c43129d6ed7f8cb4715c6f2410
Anujsingh9767 <singhanuj0990@gmail.com>
2025-05-17 22:09:05 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ;
+using lli=long long;
+using state = pair<lli,lli>;
+# define F first
+# define S second
+# define Mp make_pair
+# define INF 1e9 
+
+int main(){
+
+    ios_base::sync_with_stdio(0);srand(time(0));
+    cin.tie(0);
+    cout.tie(0);
+    
+
+    return 0;
+}
